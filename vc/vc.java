edf952681df41c833f6e06266822b5527ead1edb
Mahesh Vangala <vangalamaheshh@gmail.com>
2017-08-27 07:00:24 -0400

    added bwa mem command

--- vc/vc.java
+++ vc/vc.java
@@ -25,8 +25,8 @@ public class vc implements WorkflowDefn {
   static final String BWA_IMAGE = "docker.io/mvangala/bioifx_alignment_bwa:latest";
   
   static WorkflowArgs workflowArgs = ArgsBuilder.of()
-	  .input("BwaMem.sample_name", "${sample_name}")
-	  .build();
+    .input("BwaMem.sample_name", "${sample_name}")
+    .build();
   
   @Override
   public Workflow createWorkflow(String[] args) throws IOException {
@@ -40,20 +40,20 @@ public class vc implements WorkflowDefn {
   }
   
   static Task BwaMem = TaskBuilder.named("BwaMem")
-	  .input("sample_name").scatterBy("sample_name")
-	  .inputFile("left_mate")
-	  .inputFile("right_mate")
-	  .inputFolder("bwa_ref_path", "gs://pipelines-api/ref-files/Homo-sapiens/b37/BWAIndex")
-	  .preemptible(true)
-	  .diskSize(50)
-	  .memory(8)
-	  .cpu(4)
-	  .docker(BWA_IMAGE)
-	  .script(
-	    "set -o pipefail\n" +
-      "echo ${left_mate} >&2\n" +
-      "echo ${right_mate} >&2\n" +
-      "ls ${bwa_ref_path} >&1\n"
-	  )
-	  .build();
+    .input("sample_name").scatterBy("sample_name")
+    .inputFile("left_mate")
+    .inputFile("right_mate")
+    .inputFolder("bwa_ref_path", "gs://pipelines-api/ref-files/Homo-sapiens/b37/BWAIndex")
+    .outputFile("bwa_out_sam", "${BwaMem.sample_name}.sam")
+    .preemptible(true)
+    .diskSize(50)
+    .memory(16)
+    .cpu(4)
+    .docker(BWA_IMAGE)
+    .script(
+      "set -o pipefail\n" +
+      "bwa mem -t 4 -R '@RG\\tID:${sample_name}\\tPU:${sample_name}\\tSM:{sample_name}\\tPL:ILLUMINA\\tLB:${sample_name}' \\\n" +
+      "${bwa_ref_path}/b37 ${left_mate} ${right_mate} 1>${bwa_out_sam}"
+    )
+    .build();
 }
